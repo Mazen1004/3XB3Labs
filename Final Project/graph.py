3f59d84b97a61b1c8e6c7b35a9c63e1f851915cf
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2023-11-30 17:07:38 -0500

    added graph.py

--- Final Project/graph.py
+++ Final Project/graph.py
@@ -1,15 +1,34 @@
 class Graph:
+    def are_connected(self, node1, node2):
+        pass
+     
+    def get_adj_nodes(self, node):
+        pass
+
+    def add_node(self, node):
+        pass
+
+    def add_edge(self, start, end, w):
+        pass
+
+    def get_num_of_nodes(self):
+        pass
+
+    def w(self, node1, node2):
+        pass
+    
+class WeightedGraph(Graph):
     def __init__(self):
         self.adj = {}
         self.weights = {}
-
+        
     def are_connected(self, node1, node2):
         for neighbour in self.adj[node1]:
             if neighbour == node2:
                 return True
         return False
 
-    def adjacent_nodes(self, node):
+    def get_adj_nodes(self, node):
         return self.adj[node]
 
     def add_node(self, node):
@@ -19,14 +38,13 @@ class Graph:
         if node2 not in self.adj[node1]:
             self.adj[node1].append(node2)
         self.weights[(node1, node2)] = weight
+        
+    def get_number_of_nodes(self):
+        return len(self.adj)
 
     def w(self, node1, node2):
         if self.are_connected(node1, node2):
             return self.weights[(node1, node2)]
-
-    def number_of_nodes(self):
-        return len(self.adj)
-    
-class WeightedGraph(Graph):
-     def w(self, node1, node2):
          
+class HeuristicGraph(WeightedGraph):
+    def get_heuristic(): dict[int,float]
